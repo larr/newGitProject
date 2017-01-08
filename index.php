16ad4bb32de5dd2d3774104978cb262fbab51616
larr <cadutdutjedd@gmail.com>
2017-01-08 14:56:50 +0800

    added index.html

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>A Github lesson</title>
+</head>
+<body>
+	<h1></h1>
+	<ul>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
+</body>
+</html>
